3f6814f882d6b5bce641e58a156ddd22ba43c41b
irsyaduddin123 <jazim287@gmail.com>
2024-03-15 11:30:00 +0700

    nambahin cek inputan agar tidak kosong

--- js/script.js
+++ js/script.js
@@ -3,26 +3,36 @@
 // 2. isian pekerjaan
 // 3. isian hobby
 
-function dataPerson(){
+document.getElementById('frm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    // cek inputan kosong apa gak
+    let nama = this.nama.value.trim();
+    let pekerjaan = this.pekerjaan.value.trim();
+    let hobby = this.hobby.value.trim();
+
+    if (nama === '' || pekerjaan === '' || hobby === '') {
+        alert('Silakan isi semua formulir untuk mendapatkan hasil yang memuaskan wakaka.');
+        return;
+    }
+    
+    dataPerson();
+    this.reset(); // membersikan form inputan
+});
+
+function dataPerson() {
     let forms = document.getElementById('frm');
     let nama = forms.nama.value;
     let pekerjaan = forms.pekerjaan.value;
     let hobby = forms.hobby.value;
-    
+
     let hasilElement = document.getElementById('hasil');
     hasilElement.innerHTML = `
     <p>Nama: ${nama}</p>
     <p>Pekerjaan: ${pekerjaan}</p>
     <p>Hobby: ${hobby}</p>
-    `;}
-    
-    // Melampirkan event listener untuk formulir
-    document.getElementById('frm').addEventListener('submit', function(e) {
-        // Menghentikan perilaku bawaan formulir untuk mencegah refresh halaman
-        e.preventDefault();
-        
-        // Memanggil fungsi dataPerson untuk menampilkan hasil
-        dataPerson();
-    });
+    `;
+}
+
 
 
