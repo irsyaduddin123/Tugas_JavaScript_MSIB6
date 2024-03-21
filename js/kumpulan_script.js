bd05faedbdd164c8e4ef8315e5a46b5307309c23
irsyaduddin123 <jazim287@gmail.com>
2024-03-22 02:50:55 +0700

    Tugas 6 Web sederhana

---
+++ js/kumpulan_script.js
@@ -0,0 +1,15 @@
+
+    document.write('<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>');
+    document.write('<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>');
+    document.write('<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>');
+    document.write('<script src="https://code.jquery.com/jquery-3.7.1.js"></script>');
+    document.write('<script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>');
+    document.write('<script src="https://cdn.datatables.net/buttons/3.0.1/js/dataTables.buttons.js"></script>');
+    document.write('<script src="https://cdn.datatables.net/buttons/3.0.1/js/buttons.dataTables.js"></script>');
+    document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>');
+    document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>');
+    document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>');
+    document.write('<script src="https://cdn.datatables.net/buttons/3.0.1/js/buttons.html5.min.js"></script>');
+    document.write('<script src="https://cdn.datatables.net/buttons/3.0.1/js/buttons.print.min.js"></script>');
+
+   
